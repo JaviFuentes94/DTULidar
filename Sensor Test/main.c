96487815cebd088ba890198ac6b7a3f8926f7a76
chrische-xx <christoph-weiser@arcor.de>
2017-11-28 13:15:03 +0100

    spliting into two different test folders

---
+++ Sensor Test/main.c
@@ -0,0 +1,83 @@
+
+
+
+#include <main.h>
+
+#define CHANNEL_LONG_RANGE  4
+#define CHANNEL_SHORT_RANGE 6
+
+unsigned int16 readInternalAdc(unsigned int8 channel)
+{
+  set_adc_channel(channel);
+  delay_us(20);
+
+  read_adc(ADC_START_ONLY); // start the ADC for conversion
+  
+  int1 done = adc_done();   // Poll on ADC result
+  while(!done) 
+  {
+    // wait until measurement is ready
+    done = adc_done();
+  }
+  
+  // read conversion result
+  return((int16)read_adc(ADC_READ_ONLY));
+}
+
+//unsigned int16 readLongRangeSensor()
+float readLongRangeSensor()
+{
+  unsigned int16 iSensorRead;
+  iSensorRead = readInternalAdc(CHANNEL_LONG_RANGE);
+  
+  if(iSensorRead >= 1000 || iSensorRead <= 573 ){
+   iSensorRead = 0;
+  }
+  else
+  {
+      iSensorRead = 122f / (( 2.5f * ((float) iSensorRead) ) / 1024f - 1.28f);
+      /* Add conversion formulas here */
+      //  return ((275 * iSensorRead) / 1025);
+      //  return ((float) iSensorRead) * 0.268f;
+      printf("iSensorRead: %Lu \t in volt: %f \t\t", iSensorRead, (iSensorRead * 2.5f) / 1024f );
+   
+  }
+   return iSensorRead;
+}
+
+unsigned int16 readShortRangeSensor()
+{
+  unsigned int16 iSensorRead;
+  iSensorRead = readInternalAdc(CHANNEL_SHORT_RANGE);
+  /* Add conversion formulas here */
+  return iSensorRead;
+}
+
+void init()
+{
+  setup_oscillator(OSC_8MHZ,2);
+ 
+  /* Setup the  ADC to 2.5V */
+  //setup_vref(VREF_LOW | 0x0C);
+   setup_vref(0xEC);
+  setup_adc(ADC_CLOCK_DIV_32);
+  setup_adc_ports (sAN4 | sAN6 | VSS_VREF);
+   
+  /* Initially start with channel 6 */
+  set_adc_channel(6);
+}
+
+
+void main()
+{
+  init();
+
+  while(1)
+  {
+//   printf("Long range read: %lu\n", readLongRangeSensor());
+   printf("Long range read: %f\n", readLongRangeSensor());    
+    
+//    printf("Short range read: %lu\n\n", readShortRangeSensor());
+    delay_ms(20);
+  }
+}
